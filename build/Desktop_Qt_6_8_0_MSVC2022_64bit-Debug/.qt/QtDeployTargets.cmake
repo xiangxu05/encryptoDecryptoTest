3f3de2061648cfebe8af74e6aba0b67694c6fe67
xiangxu05 <540122281@qq.com>
2025-03-11 15:35:04 +0800

    Update .gitignore and remove build from tracking

--- build/Desktop_Qt_6_8_0_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_encryptoDecrypto_FILE D:/Jobs/QT/encryptoDecrypto/encryptoDecrypto/build/Desktop_Qt_6_8_0_MSVC2022_64bit-Debug/encryptoDecrypto.exe)
-set(__QT_DEPLOY_TARGET_encryptoDecrypto_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_encryptoDecrypto_RUNTIME_DLLS C:/Qt/6.8.0/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.8.0/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.8.0/msvc2022_64/bin/Qt6Cored.dll)
